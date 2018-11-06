d9dcc047b62a9f8a7ede414309f4930bebb56a4d
admin <2237820489@qq.com>
2018-11-06 14:47:23 +0800

    提交mybatis逆向工程

---
+++ MybatisUtils/src/main/java/com/itheima/dao/AccountMapper.java
@@ -0,0 +1,17 @@
+package com.itheima.dao;
+
+import com.itheima.domain.Account;
+
+public interface AccountMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Account record);
+
+    int insertSelective(Account record);
+
+    Account selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Account record);
+
+    int updateByPrimaryKey(Account record);
+}
